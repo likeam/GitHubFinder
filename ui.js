8b904312890bba86e05660a57b31d9a7488fd1fc
AbdulRehman <likeam99@gmail.com>
2022-06-21 23:55:12 +0500

    user interfrance

--- ui.js
+++ ui.js
@@ -3,6 +3,16 @@ class UI {
         this.profile = document.getElementById('profile');
     }
     showProfile(user){
-        console.log(user);
+        this.profile.innerHTML = `
+            <div class="card card-body mb3">
+                <div class= "row">
+                    <div class= "col-md-3">
+                        <img class= "img-fluid mb-2" src= "${user.avatar_url}">
+                        <a href= "${user.html_url}" target= "_blank">
+                    </div>
+                    
+                
+        
+        `;
         }
 }
